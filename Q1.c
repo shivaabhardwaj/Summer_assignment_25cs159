dad6d061abf55370ffcc81f6a73117a3ce687e40
shivaabhardwaj <contactshivabhardwaj@gmail.com>
2026-06-10 12:17:34 +0530

    Upload

---
+++ Q1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+ int n;
+ int sum;
+ printf("Enter the value of N: ");
+ scanf("%d", &n);
+ sum = n * (n + 1 ) / 2;
+ printf("Sum of first %d natural numbers = %d\n", n, sum);
+ return 0; 
+ }
